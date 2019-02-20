62d31400aa91be841b900c543b4170e271aaf062
AKHTARHammad <37636526+AKHTARHammad@users.noreply.github.com>
2019-02-20 11:06:57 +0100

    Update config.php

--- config.php
+++ config.php
@@ -2,21 +2,18 @@
 /*
 // mysql_connect("database-host", "username", "password")
 $conn = mysql_connect("localhost","root","root") 
-            or die("cannot connected");
- 
+			or die("cannot connected");
 // mysql_select_db("database-name", "connection-link-identifier")
 @mysql_select_db("test",$conn);
 */
- 
 /**
  * mysql_connect is deprecated
  * using mysqli_connect instead
  */
-  
 $databaseHost = 'mysqlhost';
-$databaseName = 'softwareCompany';
+$databaseName = 'test';
 $databaseUsername = 'root';
 $databasePassword = 'root';
- 
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+ 
 ?>
